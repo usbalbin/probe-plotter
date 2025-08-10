a93b16dff204c32af5fc84b63b57df8f75838728
Albin Hedman <albin9604@gmail.com>
2025-08-10 13:31:33 +0200

    Update example

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -11,14 +11,14 @@ use probe_plotter::{make_metric, make_setting};
 fn main() -> ! {
     defmt::println!("Running...");
     let mut sawtooth = make_metric!(SAWTOOTH: i32 = 42, "(SAWTOOTH / 10) % 100").unwrap();
-    defmt::println!("foo initialized to: {}", sawtooth.get());
+    defmt::println!("sawtooth initialized to: {}", sawtooth.get());
     let mut sine = make_metric!(SINE: i32 = 42, "100 * sin(2 * pi * SINE / 4000)").unwrap();
 
     let mut setting_roundtrip =
         make_metric!(SETTING_ROUNDTRIP: i8 = 0, "SETTING_ROUNDTRIP").unwrap();
 
     // Allow values -1..=7, step by 2, so {-1, 1, 3, 5, 7}
-    let mut setting = make_setting!(SETTING: i8 = 42, -1..=7, 2).unwrap();
+    let mut setting = make_setting!(SETTING: i8 = 5, -1..=7, 2).unwrap();
 
     loop {
         for i in 0..i32::MAX {
